d173e3cedced466aa853d8d151b17a0ca29416d3
henry00000 <henry31926493@gmail.com>
2024-11-19 23:21:39 +0800

    183. Customers Who Never Order

--- Database Solution/584. Find Customer Referee.sql
+++
@@ -1,3 +0,0 @@
--- SELECT name from Customer where referee_id is Null or referee_id != 2
-SELECT name from Customer where referee_id is Null or referee_id <> 2
--- SELECT name from Customer where referee_id is NULL or referee_id = 1
