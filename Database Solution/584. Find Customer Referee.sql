dc77fbc781ae4a27d7c35310712b1661e64fc73c
henry00000 <henry31926493@gmail.com>
2024-11-19 22:26:03 +0800

    584. Find Customer Referee

---
+++ Database Solution/584. Find Customer Referee.sql
@@ -0,0 +1,3 @@
+-- SELECT name from Customer where referee_id is Null or referee_id != 2
+SELECT name from Customer where referee_id is Null or referee_id <> 2
+-- SELECT name from Customer where referee_id is NULL or referee_id = 1
